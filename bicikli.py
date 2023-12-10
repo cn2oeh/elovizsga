4543e4558483b96f7be25edb1b2551d94f86cd76
cn2oeh <cn2oeh@neptun.gde.hu>
2023-12-10 15:32:18 +0100

    harmadik

--- bicikli.py
+++ bicikli.py
@@ -16,4 +16,19 @@ class HegyiBicikli(Bicikli):
     print('Ez egy hegyi bicikli')
 
 
-my_bicikli = Bicikli('kék', 100, 'van')
+class Kolcsonzo:
+    def __init__(self, biciklik:[Bicikli], kolcsonzonev: str):
+        self.biciklik = biciklik
+        self.kolcsonzonev = kolcsonzonev
+
+    def __str__(self):
+        return f"A kölcsönző neve {self.kolcsonzonev}."
+
+
+my_biciklik = [Bicikli("Országúti bicikli", 100000, "jó állapotú" ), Bicikli("Hegyi bicikli",150000, "lestrapált")]
+my_kolcsonzo = Kolcsonzo([my_biciklik],"Tekergő")
+print(my_kolcsonzo)
+
+#my_bicikli = Bicikli('kék', 100, 'van')
+
+
